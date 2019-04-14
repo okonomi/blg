a49b899ad8c000a11ca4865242919c3e227c3a93
okonomi <okonomi@oknm.jp>
2019-04-14 19:15:12 +0900

    edit and update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title, :content))
+  end
 end
