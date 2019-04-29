241b30b5cbb21c94f0cdab7c2bbdbbd1a06de6a2
okonomi <okonomi@oknm.jp>
2019-04-29 22:44:01 +0900

    switch sqlite to postgresql (#11)
    because heroku database is postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_24_140257) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
