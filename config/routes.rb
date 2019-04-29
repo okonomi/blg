8cf7667579cda8e9af619e44581b2246386c9e30
okonomi <okonomi@oknm.jp>
2019-04-29 19:18:28 +0900

    simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/auth/github/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :posts, only: [:show, :edit, :update, :new, :create, :destroy]
+  resources :posts
 
   direct(:login) { "/auth/github?origin=#{request.original_url}" }
 
