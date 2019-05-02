40db74c320485c17eb9f5622addad0337a6e2bab
okonomi <okonomi@oknm.jp>
2019-05-02 20:35:06 +0900

    remove content column from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_100929) do
+ActiveRecord::Schema.define(version: 2019_05_02_113042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_05_01_100929) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "posted_at", precision: 6
