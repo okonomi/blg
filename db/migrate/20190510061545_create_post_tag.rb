d379bde66c853e7c85da480e791b049e1dfdb9e0
okonomi <okonomi@oknm.jp>
2019-05-10 15:31:18 +0900

    introduce tag for blog post

---
+++ db/migrate/20190510061545_create_post_tag.rb
@@ -0,0 +1,8 @@
+class CreatePostTag < ActiveRecord::Migration[6.0]
+  def change
+    create_table :post_tags do |t|
+      t.references :post, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
+    end
+  end
+end
