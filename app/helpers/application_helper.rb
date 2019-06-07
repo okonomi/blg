c2dd0683262f6fbc289eb4713fcf20c4f3d81665
okonomi <okonomi@oknm.jp>
2019-06-07 13:41:48 +0900

    add og meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def page_title(title)
-    [title, ENV.fetch("BLOG_TITLE")].reject(&:blank?).join(" | ")
-  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,22 +1,4 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "#page_title" do
-    subject do
-      ENV["BLOG_TITLE"] = blog_title
-      page_title(title)
-    end
-
-    let(:blog_title) { "blog" }
-
-    context "when present title" do
-      let(:title) { "awesome post" }
-      it { is_expected.to eq "awesome post | blog" }
-    end
-
-    context "when blank title" do
-      let(:title) { "" }
-      it { is_expected.to eq "blog" }
-    end
-  end
 end
