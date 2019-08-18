f3657583cc54fb1ebafac97ad87232f6223f055b
okonomi <okonomi@oknm.jp>
2019-08-18 13:33:49 +0900

    fix login flow

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,6 +1,14 @@
 class AuthsController < ApplicationController
   layout false
 
+  before_action :already_logged_in
+
   def show
+  end
+
+  private
+
+  def already_logged_in
+    redirect_to root_path if logged_in?
   end
 end
