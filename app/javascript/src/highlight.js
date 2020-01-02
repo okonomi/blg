30ba80c5463ff6565436c2da639137bf9104b1c5
okonomi <okonomi@oknm.jp>
2020-01-02 20:49:01 +0900

    style: 💄 Improve syntax highlighting

--- app/javascript/src/highlight.js
+++ app/javascript/src/highlight.js
@@ -1,6 +1,16 @@
 import hljs from 'highlight.js'
 import 'highlight.js/styles/default.css'
 
+hljs.configure({
+  languages: [
+    'shell',
+    'ruby',
+    'html',
+    'yaml',
+    'dockerfile'
+  ]
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre').forEach(block => {
     hljs.highlightBlock(block)
