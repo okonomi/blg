f1f4bc1e461d4291ec2c18942ad28aab04f5a755
okonomi <okonomi@oknm.jp>
2021-12-22 01:07:11 +0900

    load some js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,9 @@ import "trix"
 import "@rails/actiontext"
 import Rails from "@rails/ujs"
 
+import './src/bulma-navbar'
+import './src/bulma-notification'
+import './src/trix'
+import './src/highlight'
+
 Rails.start()
