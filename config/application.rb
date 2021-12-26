a910ad2d96a30cc8ddcb680f6b0ad2ce319b3592
okonomi <okonomi@oknm.jp>
2021-12-26 23:56:33 +0900

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Blg
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,8 +18,5 @@ module Blg
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = ENV["TZ"]
-
-    config.action_view.prefix_partial_path_with_controller_namespace = false
   end
 end
