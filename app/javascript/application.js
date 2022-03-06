05337676936f7fef5227cf0f452b458f5bb69c09
okonomi <okonomi@oknm.jp>
2022-03-06 19:49:53 +0900

    feat: bin/rails importmap:install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,1 @@
-// Entry point for the build script in your package.json
-import "trix"
-import "@rails/actiontext"
-import Rails from "@rails/ujs"
-
-import './src/bulma-navbar'
-import './src/bulma-notification'
-import './src/trix'
-import './src/highlight'
-
-Rails.start()
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
