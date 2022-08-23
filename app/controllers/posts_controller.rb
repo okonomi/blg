5b9edb1c8cefccfd22b806cfdca696a058ce5e99
okonomi <okonomi@oknm.jp>
2022-08-23 23:47:58 +0900

    add posts show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
   end
+
+  def show
+
+  end
+
+  private
+
+    def set_post
+      @post = Post.find_by(uid: params[:uid])
+    end
 end
