7b7a795f83f307949527ad4afe77e79309e44b79
okonomi <okonomi@oknm.jp>
2022-08-23 23:54:50 +0900

    add posts edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,24 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: t("flash.post_updated")
+    end
   end
 
   private
 
     def set_post
       @post = Post.find_by(uid: params[:uid])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content, :published_at, :status)
     end
 end
