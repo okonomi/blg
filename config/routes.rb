ca96da15195a24df84a706256a76bdf77b0f2736
okonomi <okonomi@oknm.jp>
2022-08-24 22:23:01 +0900

    add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resource :auth, only: :show
+
   resources :posts, param: :uid
+  direct(:login) { "/auth/github?origin=#{request.original_url}" }
 
   root "posts#index"
 end
