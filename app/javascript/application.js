409145a518e863b2a38ab5f6b92d0ba59c2c6ca1
okonomi <okonomi@oknm.jp>
2022-08-25 22:48:44 +0900

    remove unnecessary script

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "trix"
 import "@rails/actiontext"
 import Rails from "@rails/ujs"
 
-import './src/bulma-navbar'
 import './src/bulma-notification'
 import './src/trix'
 import './src/highlight'
